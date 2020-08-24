6f35ec413f0c7dd5a4c3d23a3409648f08d873bf
Hyojin Kim <hyojins.kim@samsung.com>
2020-08-24 15:59:19 +0900

    Update

--- container/js/main.js
+++ container/js/main.js
@@ -26,13 +26,13 @@
     var socket = null;
     var iconDimTimer = null;
     var isLoadingContents = false;
-    var CONTENT_PATH = '/home/owner/share/tmp/sdk_tools/mon1WITs';
-    var CONTENT_SRC = '/home/owner/share/tmp/sdk_tools/mon1WITs/index.html';
+    var CONTENT_PATH = '/home/owner/share/tmp/sdk_tools/thu2WITs';
+    var CONTENT_SRC = '/home/owner/share/tmp/sdk_tools/thu2WITs/index.html';
     var IP = 'http://10.88.106.190';
-    var PORT = '5085';
+    var PORT = '5150';
     var CONNECTED = 'Connected';
     var DISCONNECTED = 'Disconnected';
-    var HOST_BASE_CONTENT_PATH = 'E:/dev/tizenworkspace/mon1';
+    var HOST_BASE_CONTENT_PATH = 'E:/dev/tizenworkspace/thu2';
 
     window.onload = function () {
         console.log('onload!!!');
